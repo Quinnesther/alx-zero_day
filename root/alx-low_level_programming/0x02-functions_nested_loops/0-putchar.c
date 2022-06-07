98fb01ded162cb7e73bc797706c22362550efd35
Quinnesther <chidinmachizoma2019@gmail.com>
2022-06-07 16:09:47 -0700

    0-putchar.c

---
+++ root/alx-low_level_programming/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print the word "main"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char p[] = "main";
+
+for (i = 0; i < 8; i++)
+{
+_putchar(p[i]);
+}
+
+_putchar('\n');
+return (0);
+}
